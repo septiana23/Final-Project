9d043fc2cb02ffd2438604b108c4aba0a17ffb27
septiana23 <110086297+septiana23@users.noreply.github.com>
2022-08-03 06:42:36 +0700

    Add files via upload

---
+++ OrangeHRM/Time/Time_SearchTimesheets.py
@@ -0,0 +1,32 @@
+import unittest
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class TestTime(unittest.TestCase): 
+
+    def setUp(self):    
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    def test_search_timesheets(self): 
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/index.php/time/viewEmployeeTimesheet") # buka situs
+        browser.maximize_window()
+        time.sleep(3)
+        browser.find_element(By.ID,"txtUsername").send_keys("Admin") # isi username
+        browser.find_element(By.ID,"txtPassword").send_keys("admin123") # isi password
+        time.sleep(1)
+        browser.find_element(By.ID,"btnLogin").click() # klik tombol log in
+        time.sleep(3)
+        browser.find_element(By.ID,"employee").click()
+        browser.find_element(By.ID,"employee").send_keys("Sara Tencrady") 
+        time.sleep(3)
+        browser.find_element(By.ID,"btnView").click()
+        time.sleep(3)
+    
+    def tearDown(self): 
+        self.browser.close() 
+
+if __name__ == "__main__": 
+    unittest.main()
